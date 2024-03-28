ead374abc8e57bcb0c9d2e4bdef2818a927ca3b0
William Di Luigi <williamdiluigi@gmail.com>
2024-03-28 17:03:24 +0100

    Updates

---
+++ _app/immutable/chunks/paths.4bc93115.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_19wvyk6)==null?void 0:s.base)??"/itacpc.github.io";var t;const e=((t=globalThis.__sveltekit_19wvyk6)==null?void 0:t.assets)??a;export{e as a,a as b};
